63fbcd793dbbd81b8e6ec66eb367e48deacbe932
KomoriWu <tmwuzhen@qq.com>
2017-12-12 17:38:33 +0800

    2017-12-12 base module

--- app/src/main/java/com/komori/eyepetizer/MainActivity.kt
+++ app/src/main/java/com/komori/eyepetizer/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.komori.eyepetizer
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.komori.home.HomeActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
